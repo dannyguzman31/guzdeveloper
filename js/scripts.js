e5b90e2549658df31ad1893ada7a68833cad3cb3
Daniel Guzman <42854222+dannyguzman31@users.noreply.github.com>
2021-09-21 11:23:37 -0600

    Added dark-mode toggle feature

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-
+// Typewriter function
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
@@ -44,3 +44,21 @@ typewriter.typeString("Hello! I'm Daniel Guzmán")
     .typeString('<strong>Software Developer!</strong>')
     .pauseFor(2500)
     .start();
+
+// Dark mode toggle
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
