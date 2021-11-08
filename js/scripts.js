9c7a8d6f8521986e6e44754c4f91e564b6353f6b
Daniel Guzman <42854222+dannyguzman31@users.noreply.github.com>
2021-11-08 14:17:03 -0700

    updates to features

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,59 @@
 //
 // Scripts
 //
-
-
-
 window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector('#sideNav');
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#sideNav',
+      offset: 74,
     });
-
+  };
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function(responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
 });
+
 
 // Typewriter function
 var app = document.getElementById('app');
-
 var typewriter = new Typewriter(app, {
-    loop: true
+  loop: true
 });
-
 typewriter.typeString("Hello! I'm Daniel")
-    .pauseFor(3000)
-    .deleteAll()
-    .typeString("A Web Developer!")
-    .pauseFor(3000)
-    .deleteChars(14)
-    .typeString('<strong>Software Developer!</strong>')
-    .pauseFor(3000)
-    .start();
+  .pauseFor(3000)
+  .deleteAll()
+  .typeString("A Web Developer!")
+  .pauseFor(3000)
+  .deleteChars(14)
+  .typeString('<strong>Software Developer!</strong>')
+  .pauseFor(3000)
+  .start();
 
-// Dark mode toggle
+
+// Dark Mode
 const options = {
   bottom: '64px', // default: '32px'
   right: 'unset', // default: '32px'
   left: '32px', // default: 'unset'
   time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  backgroundColor: '#fff', // default: '#fff'
+  buttonColorDark: '#100f2c', // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
   saveInCookies: false, // default: true,
   label: '🌓', // default: ''
   autoMatchOsTheme: true // default: true
 }
-
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
